7bd0f99cd225488ebb100caae0a55d6b0e0e3e4c
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2025-02-12 09:47:51 +0100

    feat: add class with default getter setter (based on the diagram)

---
+++ src/Cart/Cart.js
@@ -0,0 +1,47 @@
+/**
+ * @name Cart
+ * @desciption This class represents a cart 
+ * @author Nathan Chauveau
+ * @version 1.0
+ * @date 12/02/2025
+ */
+
+"use strict";
+
+const EmptyCartException = require("./EmptyCartException.js");
+const UpdateCartException = require("./UpdateCartException.js");
+
+module.exports = class Cart {
+
+    //region private attributes
+    //endregion private attributes
+
+    //region public methods
+
+    constructor(cartItems) {
+        this._cartItems = [];
+    }
+
+    get items() {
+        if(this._cartItems.length === 0) {
+            throw new EmptyCartException();
+        }
+        return this._cartItems;
+    }
+    set items(value) {
+        if(value.length === 0) {
+            throw new EmptyCartException();
+        }
+        this._cartItems = value;
+    }
+    get total() {
+        if(this._cartItems.length === 0) {
+            throw new EmptyCartException();
+        }
+        let total = 0;
+        this._cartItems.forEach(cartItem => {
+            total += cartItem.total;
+        });
+        return total;
+    }
+}
